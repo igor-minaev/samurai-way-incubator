c08da3a199f09242d5d1f011109e7ed14ea95b59
Igor Minayeu <igor.minayeu@gmail.com>
2023-10-03 20:20:25 +0300

    lesson 11 (social network, grid)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,56 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className="app-wrapper">
+            <header className="header">
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUtxIJcJTI8MUjqaBDRUD11pytL6p5oE0SUA&usqp=CAU"
+                    alt="logo"/>
+            </header>
+            <nav className="nav">
+                <div>
+                    <a>Profile</a>
+                </div>
+                <div>
+                    <a>Messages</a>
+                </div>
+                <div>
+                    <a>News</a>
+                </div>
+                <div>
+                    <a>Music</a>
+                </div>
+                <div>
+                    <a>Settings</a>
+                </div>
+            </nav>
+            <div className="content">
+                <div>
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjXeGwxae3il227GwauWDhoncozPqrqfMEbA&usqp=CAU"
+                        alt="image"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
