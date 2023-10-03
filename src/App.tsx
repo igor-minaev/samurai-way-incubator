974b4fa76ece37e29b9c7e1bb8516323a62dd84e
Igor Minayeu <igor.minayeu@gmail.com>
2023-10-03 20:38:10 +0300

    lesson 12 (divide to components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img
-                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUtxIJcJTI8MUjqaBDRUD11pytL6p5oE0SUA&usqp=CAU"
-                    alt="logo"/>
-            </header>
-            <nav className="nav">
-                <div>
-                    <a>Profile</a>
-                </div>
-                <div>
-                    <a>Messages</a>
-                </div>
-                <div>
-                    <a>News</a>
-                </div>
-                <div>
-                    <a>Music</a>
-                </div>
-                <div>
-                    <a>Settings</a>
-                </div>
-            </nav>
-            <div className="content">
-                <div>
-                    <img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjXeGwxae3il227GwauWDhoncozPqrqfMEbA&usqp=CAU"
-                        alt="image"/>
-                </div>
-                <div>
-                    ava + description
-                </div>
-                <div>
-                    My posts
-                    <div>
-                        New post
-                    </div>
-                    <div>
-                        <div>
-                            post 1
-                        </div>
-                        <div>
-                            post 2
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Header/>
+            <Navbar/>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUtxIJcJTI8MUjqaBDRUD11pytL6p5oE0SUA&usqp=CAU"
+                alt="logo"/>
+        </header>
+    )
+}
+
+export default Header;
