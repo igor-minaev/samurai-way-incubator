b7c3aa28970048c513c763185fef3994a3edf7b7
Igor Minayeu <igor.minayeu@gmail.com>
2023-10-12 19:44:41 +0300

    lesson 13-14 (css-modules)

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import s from "./Profile.module.css"
 
 const Profile = () => {
     return (
-        <div className="content">
+        <div className={s.content}>
             <div>
                 <img
                     src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjXeGwxae3il227GwauWDhoncozPqrqfMEbA&usqp=CAU"
@@ -17,10 +18,10 @@ const Profile = () => {
                     New post
                 </div>
                 <div>
-                    <div>
+                    <div className={s.item}>
                         post 1
                     </div>
-                    <div>
+                    <div  className={s.item}>
                         post 2
                     </div>
                 </div>
